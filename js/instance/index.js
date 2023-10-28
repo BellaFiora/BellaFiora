99d8e4fef05756b9b517cfd0fa29e6e42fccb86e
pupariaa <maxime.zorloni@sfr.fr>
2023-10-28 03:53:52 +0200

    update output class

--- js/instance/index.js
+++ js/instance/index.js
@@ -1,13 +1,15 @@
 const CommandParser = require('./Modules/CommandParser');
 const commandParser = new CommandParser();
 
-const user_command = '!bm bpm100';
-const from = 'Maxime';
+const user_command = '!bm --od 8 --hp 7 --cs 4.5 --ar 9 --status ranked --mapper sotarks';
+const from = 'Puparia';
 const result = commandParser.parseCommand(user_command, from);
-if (result.error) {
+if (result.error > 0) {
   console.log(result.error);
+  console.table(result.output)
 } else {
-  console.log(result);
+  console.table(result);
+  console.table(result.filters)
 }
 
 
