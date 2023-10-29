fbab131e4ea7a50000cef4bdff54eea6e29e7bd6
pupariaa <maxime.zorloni@sfr.fr>
2023-10-29 14:43:23 +0100

    Add Metrics Time Execution

--- js/instance/Modules/FiltersParser.js
+++ js/instance/Modules/FiltersParser.js
@@ -1,126 +1,130 @@
+const ExecTime = require("../Metrics/Modules/ExecTime")
+const timer= new ExecTime()
+
+
 class ParseFilters {
   constructor() {
     this.options = [
       { 
         prefixAllowed: "--",
-        option: "od", 
-        format: "float",
+        option: "od", format: "float",
         threshold: 0.1, 
-        min_value: 0,
-        max_value: 10,
+        min_value: 0, max_value: 10,
         arguments: 1,
         description: "[Argument: 1, Type: Float, Range: 1-10]",
         acceptedArgument: []
-      },
-      { 
+      },{ 
         prefixAllowed: "--",
-        option: "hp", 
-        format: "float",
+        option: "hp", format: "float",
         threshold: 0.1, 
-        min_value: 0,
-        max_value: 10,
+        min_value: 0, max_value: 10,
         arguments: 1,
         description: "[Argument: 1, Type: Float, Range: 1-10]",
         acceptedArgument: []
-      },
-      { 
+      },{ 
         prefixAllowed: "--",
-        option: "cs", 
-        format: "float",
+        option: "cs", format: "float",
         threshold: 0.1, 
-        min_value: 0,
-        max_value: 10,
+        min_value: 0, max_value: 10,
         arguments: 1,
         description: "[Argument: 1, Type: Float, Range: 1-10]",
         acceptedArgument: []
-      },
-      { 
+      },{ 
         prefixAllowed: "--",
-        option: "ar", 
-        format: "float",
+        option: "ar", format: "float",
         threshold: 0.1, 
-        min_value: 0,
-        max_value: 10,
+        min_value: 0, max_value: 10,
         arguments: 1,
         description: "[Argument: 1, Type: Float, Range: 1-10]",
         acceptedArgument: []
-      },
-      { 
+      },{ 
         prefixAllowed: "--",
-        option: "sr", 
-        format: "float",
+        option: "sr", format: "float",
         threshold: 0.5, 
-        min_value: 0,
-        max_value: 10,
+        min_value: 0, max_value: 10,
         arguments: 1,
         description: "[Argument: 1, Type: Float, Range: 1-10]",
         acceptedArgument: []
-      },
-      { 
+      },{ 
         prefixAllowed: "--",
-        option: "pp", 
-        format: "float",
+        option: "pp", format: "float",
         threshold: 10, 
-        min_value: 0,
-        max_value: 4000,
+        min_value: 0, max_value: 4000,
         arguments: 1,
         description: "[Argument: 1, Type: Int, Range: 0-4000]",
         acceptedArgument: []
-      },
-      { 
+      },{ 
         prefixAllowed: "--",
-        option: "bpm", 
-        format: "float",
+        option: "bpm", format: "float",
         threshold: 10, 
-        min_value: 2,
-        max_value: 600,
+        min_value: 2, max_value: 600,
         arguments: 1,
         description: "[Argument: 1, Type: Int, Range: 2-600]",
         acceptedArgument: []
-      },
-      { 
+      },{ 
         prefixAllowed: "--",
-        option: "mapper", 
-        format: "string",
+        option: "mapper", format: "string",
         threshold: null,
-        min_value: null,
-        max_value: null,
+        min_value: null, max_value: null,
         arguments: 1,
         description: '[Argument: 1, Type: String, Format: "pseudo"]',
         acceptedArgument: []
-      },
-      { 
+      },{ 
         prefixAllowed: "--",
-        option: "s", 
-        format: "string",
+        option: "s", format: "string",
         threshold: null, 
-        min_value: null,
-        max_value: null,
+        min_value: null, max_value: null,
         arguments: 1,
         description: '[Argument: 1, Type: String, Format: "name of save"]',
         acceptedArgument: []
-      },
-      { 
+      },{ 
         prefixAllowed: "--",
-        option: "status", 
-        format: "string",
+        option: "status", format: "string",
         threshold: null, 
-        min_value: null,
-        max_value: null,
+        min_value: null,  max_value: null,
         arguments: 1,
         description: '[Argument: 1, Type: String, Options: ranked, loved, graveyard, wip]',
         acceptedArgument: ["ranked", "loved", "graveyard", "wip"]
-      },
-      { 
+      },{ 
         prefixAllowed: "--",
-        option: "help", 
-        format: "string",
+        option: "help", format: "string",
         threshold: null, 
-        min_value: null,
-        max_value: null,
+        min_value: null, max_value: null,
         arguments: 0,
         description: "Get a help",
         acceptedArgument: []
+      },{ 
+        prefixAllowed: "",
+        option: "jump", format: "string",
+        threshold: null, 
+        min_value: null, max_value: null,
+        arguments: 0,
+        description: "Jump map",
+        acceptedArgument: ['jump']
+      },{ 
+        prefixAllowed: "",
+        option: "stream",format: "string",
+        threshold: null, 
+        min_value: null, max_value: null,
+        arguments: 0,
+        description: "Stream map",
+        acceptedArgument: ['stream']
+      },{ 
+        prefixAllowed: "",
+        option: "tech", format: "string",
+        threshold: null, 
+        min_value: null, max_value: null,
+        arguments: 0,
+        description: "Stream map",
+        acceptedArgument: ['tech']
+      },{ 
+        prefixAllowed: "",
+        option: "slider", format: "string",
+        threshold: null, 
+        min_value: null,  max_value: null,
+        arguments: 0,
+        description: "Slider map",
+        acceptedArgument: ['slider']
       },
     ];
     this.parameterValues = {
@@ -142,25 +146,22 @@ class ParseFilters {
       mapper: false,
       status: false,
       s: false,
-
     };
     this.errors = [];
   }
-
-  parse(inputString) {
-
+  parse(inputString, id) {
+    timer.start('FilterParser');
     const inputArray = inputString.match(/"[^"]+"|\S+/g);
     let currentOption = null;
     for (let i = 0; i < inputArray.length; i++) {
       const currentToken = inputArray[i];
+      const optionName = currentToken.split(" ")[0];
       if (currentToken.startsWith("--")) {
-        const optionName = currentToken.split(" ")[0];
         const option = this.options.find(opt => opt.prefixAllowed + opt.option === optionName);
         if (option) {
           currentOption = option;
           let argument = null;
           const argumentIndex = i + 1;
-
           if (option.arguments > 0 && argumentIndex < inputArray.length) {
             argument = inputArray[argumentIndex];
             if (option.format === "string" && option.acceptedArgument.length > 0 && !option.acceptedArgument.includes(argument)) {
@@ -175,15 +176,26 @@ class ParseFilters {
           } else {
             this.parameterValues[`${option.option}`] = true
           }
-
-          if (option.format === "float") {
-            argument = parseFloat(argument);
-          }
-
           if (option.min_value !== null && option.max_value !== null) {
-            if (argument < option.min_value || argument > option.max_value) {
-              this.errors.push({ error: 4, error_name: `Value for ${option.option} is out of range [${option.min_value}, ${option.max_value}]. ${option.description}` });
+            if (typeof argument !== 'string') {
+              argument = argument.toString();
+            }
+            if (argument.startsWith('>')) {
+              const argValue = parseFloat(argument.substring(1));
+              this.parameterValues[`${option.option}_min`] = argValue;
+              this.parameterValues[`${option.option}_max`] = option.max_value;
+            } else if (argument.startsWith('<')) {
+              const argValue = parseFloat(argument.substring(1));
+              console.log(argValue)
+              this.parameterValues[`${option.option}_min`] = 0;
+              this.parameterValues[`${option.option}_max`] = argValue;
             } else {
+              argument = parseFloat(argument);
+              if (argument < option.min_value) {
+                argument = option.min_value;
+              } else if (argument > option.max_value) {
+                argument = option.max_value;
+              }
               this.parameterValues[`${option.option}_min`] = (argument - option.threshold).toFixed(1);
               this.parameterValues[`${option.option}_max`] = (argument + option.threshold).toFixed(1);
             }
@@ -192,10 +204,21 @@ class ParseFilters {
         } else {
           this.errors.push({ error: 2, error_name: `Invalid option: ${optionName}` });
         }
-      } 
+      } else {
+        const option = this.options.find(opt => opt.option === optionName);
+        if(option){
+          if(option.acceptedArgument[0].toLowerCase() === optionName.toLowerCase()){
+            this.parameterValues[`${option.option}`] = true
+          } else {
+            this.errors.push({ error: 2, error_name: `Invalid option: ${optionName}`});
+          }
+        } else {
+          this.errors.push({ error: 2, error_name: `Invalid option: ${optionName}` });
+        }
+      }
     }
-    const result = { parameterValues: this.parameterValues, errors: this.errors };
-    console.table(result)
+    const result = { parameterValues: this.parameterValues, errors: this.errors , id: id };
+    timer.stop('FilterParser');
     return result
   }
 }
