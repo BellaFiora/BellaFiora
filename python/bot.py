6f6eba7c7f6eff408356159de297ef6f98525228
Bob de Boisvilliers <Bo_wood@outlook.fr>
2023-11-07 09:48:07 +0100

    added some basic commands for example

--- python/bot.py
+++ python/bot.py
@@ -8,10 +8,34 @@ class BellaFiora(osu_irc.Client):
         await self.joinChannel("#osu")
         print(f'{self.nickname} connected and joined channel #osu')
 
-    async def bmCommand(self, pseudo:str, command:str) -> None:
-    	response = bm_command(command)
+    async def helpCommand(self, pseudo:str, command:str) -> None:
+        response = help_command(command)
+        await self.sendContent(f"PRIVMSG {pseudo} :{response}\r\n")
+
+    async def pingCommand(self, pseudo:str, command:str) -> None:
+    	response = ping_command(command)
     	await self.sendContent(f"PRIVMSG {pseudo} :{response}\r\n")
+    
+    async def echoCommand(self, pseudo:str, command:str) -> None:
+        response = echo_command(command)
+        await self.sendContent(f"PRIVMSG {pseudo} :{response}\r\n")
+    
+    async def bmCommand(self, pseudo:str, command:str) -> None:
+        response = bm_command(command)
+        await self.sendContent(f"PRIVMSG {pseudo} :{response}\r\n")
         # await self.sendPM(pseudo, response)
+
+    async def onCommand(self, pseudo:str, command:str) -> None:
+        if command.startswith('!help'):
+            await self.helpCommand(pseudo, command)
+        elif command.startswith('!ping'):
+            await self.pingCommand(pseudo, command)
+        elif command.startswith('!echo'):
+            await self.echoCommand(pseudo, command)
+        elif command.startswith('!bm'):
+            await self.bmCommand(pseudo, command)
+        else:
+            await self.helpCommand(pseudo, command)
 
     async def onRaw(self, raw:bytes) -> None:
         string = str(raw, encoding='utf-8')
@@ -34,7 +58,7 @@ class BellaFiora(osu_irc.Client):
             else:
                 # is not an action
                 print(f"received pm \"{content}\" from {pseudo}")
-                await self.bmCommand(pseudo, content)
+                await self.onCommand(pseudo, content)
 
 def main():
     nickname = 'Bella_Fiora'
--- python/commands.py
+++ python/commands.py
@@ -1,7 +1,7 @@
 import sys, os
 from python_utils.utils.all import *
 
-def load_commands():
+def load_commands() -> None:
 	global bm_options, default_bm_arguments, default_bm_options
 	options = None
 	with open('../commands/bm_options.json', 'r') as f:
@@ -10,7 +10,7 @@ def load_commands():
 	default_bm_arguments = options["default_arguments"]
 	default_bm_options = options["default_options"]
 
-def send_to_beatmap_parser(osufile_path):
+def send_to_beatmap_parser(osufile_path:str) -> None:
 	global jsonified_bm
 	os.system('cd beatmap_parser/bin && bm_parser.exe ' + osufile_path)
 	while not os.path.exists('beatmap_parser/bin/out'):
@@ -19,7 +19,16 @@ def send_to_beatmap_parser(osufile_path):
 		jsonified_bm =json.loads(f.read())
 	os.remove('beatmap_parser/bin/out')
 
-def bm_command(string):
+def help_command(string:str) -> str:
+	return 'current commands: help, ping, echo, bm'
+
+def ping_command(string:str) -> str:
+	return 'pong'
+
+def echo_command(string:str) -> str:
+	return string[6:]
+
+def bm_command(string:str) -> str:
 	args, opts = parse(string, bm_options, False, default_bm_arguments, default_bm_options)
 	print(args, opts)
 	if opts[-1]:
