851c92c433a4f19e95e4bbef593987275b2f3312
Bob de Boisvilliers <Bo_wood@outlook.fr>
2023-11-27 00:39:29 +0100

    Update update_dockers.py

--- update_dockers.py
+++ update_dockers.py
@@ -2,36 +2,63 @@ import os, sys, time
 from py_bot.python_utils.utils.all import *
 
 st_total = time.time()
-st = time.time()
 docker_manager = DockerManager(*read_file('.ssh').split('\n')[0:5], root_depth=5)
-time_to_connect = time.time() - st
-ti = TimeIt()
 
 root = '/mnt/user/node-containers/BellaFiora/'
-sent_files = []
-nb_removed_files = 0
-nb_moved_files = 0
 commits_done = []
 updated_dockers = []
-st = time.time()
 if os.path.exists('commits_done'):
 	content = ''
 	with open('commits_done', 'r') as f:
 		content = f.read()
 	commits_done = content.split('\n')[:-1]
-ti.time += time.time() - st
-ti.measured += 1
 
 docker_names = ['private_api', 'public_api', 'bm_manager', 'js_bot', 'py_bot', 'user_manager', 'webapp']
-st = time.time()
 logs = os.popen('git log --name-status --oneline --no-decorate --format="///%H"').read()
-ti.time += time.time() - st
-ti.measured += 1
 commits = logs.split('///')[1:]
-# /////////////////////// WIP ///////////////////////
-@profile
+'''
+cmd>git log --name-status --oneline --no-decorate --format="///%H"
+///ce042254a2ce20233646c8f657d713d011e3e77c
+
+M       py_bot/python_utils/utils/web.py
+///50de47255f3c99e9ee7715b894b7b8613b55d91e
+
+M       py_bot/python_utils/utils/constants.py
+M       py_bot/python_utils/utils/web.py
+///e3101286b2b67a9b8f07480b88b0893b78350d6c
+
+R100    common/assets/34071295.jpg      common/assets/avatar.jpg
+///c52dba7a6b27c958a04c44dd4101fe5886bbbcd0
+
+A       common/assets/34071295.jpg
+///bc80dbdb990cbf3f84d375099203bf150ff156f2
+
+M       py_bot/python_utils/utils/web.py
+M       update_dockers.py
+///cbcba790c0fa7026542a4060de6ed596b46dd144
+
+M       py_bot/python_utils/utils/all.py
+A       py_bot/python_utils/utils/constants.py
+A       py_bot/python_utils/utils/io.py
+M       py_bot/python_utils/utils/os.py
+D       py_bot/python_utils/utils/unsorted.py
+M       py_bot/python_utils/utils/web.py
+M       update_dockers.py
+A       update_dockers.py.lprof
+
+commits = [
+'ce042254a2ce20233646c8f657d713d011e3e77c\n\nM\tpy_bot/python_utils/utils/web.py\n',
+'50de47255f3c99e9ee7715b894b7b8613b55d91e\n\nM\tpy_bot/python_utils/utils/constants.py\nM\tpy_bot/python_utils/utils/web.py\n',
+'e3101286b2b67a9b8f07480b88b0893b78350d6c\n\nR100\tcommon/assets/34071295.jpg\tcommon/assets/avatar.jpg\n',
+'c52dba7a6b27c958a04c44dd4101fe5886bbbcd0\n\nA\tcommon/assets/34071295.jpg\n',
+'bc80dbdb990cbf3f84d375099203bf150ff156f2\n\nM\tpy_bot/python_utils/utils/web.py\nM\tupdate_dockers.py\n',
+'cbcba790c0fa7026542a4060de6ed596b46dd144\n\nM\tpy_bot/python_utils/utils/all.py\nA\tpy_bot/python_utils/utils/constants.py\nA\tpy_bot/python_utils/utils/io.py\nM\tpy_bot/python_utils/utils/os.py\nD\tpy_bot/python_utils/utils/unsorted.py\nM\tpy_bot/python_utils/utils/web.py\nM\tupdate_dockers.py\nA\tupdate_dockers.py.lprof\n'
+]
+'''
+
 def process_commit(commit):
 	tmp = commit.split('\n')
+	'''['ce042254a2ce20233646c8f657d713d011e3e77c', '', 'M\tpy_bot/python_utils/utils/web.py', '']'''
 	commit_hash = tmp[0]
 	if commit_hash in commits_done: return
 	edits = tmp[2:-1]
@@ -40,46 +67,31 @@ def process_commit(commit):
 		action = tmp[0][0]
 		path = tmp[1]
 		full_path = path.split('/')
-		if len(full_path) <= 1: return
+		if len(full_path) <= 1: continue
 		docker = full_path[0]
+		if not docker in docker_names: continue
 		remotepath = root+path
 		if action == 'A' or action == 'M' or action == 'C':
-			if path in sent_files: return
-			if docker_manager.send(path, remotepath):
-				ti.timeit(print, 'sent', path, remotepath)
-				sent_files.append(path)
+			docker_manager.send(path, remotepath)
 		elif action == 'D':
-			if docker_manager.remove(remotepath):
-				ti.timeit(print, 'removed', remotepath)
-				nb_removed_files += 1
+			docker_manager.remove(remotepath)
 		elif action == 'T':
-			if docker_manager.remove(remotepath):
-				ti.timeit(print, 'removed', remotepath)
-				nb_removed_files += 1
-			if docker_manager.send(path, remotepath):
-				ti.timeit(print, 'sent', path, remotepath)
-				sent_files.append(path)
+			docker_manager.move(path, remotepath)
 		elif action == 'R':
 			dest = tmp[2]
 			full_dest = dest.split('/')
-			if len(full_dest) <= 1: return
+			if len(full_dest) <= 1: continue
 			docker = full_dest[0]
-			remotepath_dest = root+dest
-			if docker_manager.exists(remotepath) and docker_manager.move(remotepath, remotepath_dest):
-				ti.timeit(print, 'moved', remotepath, remotepath_dest)
-				nb_moved_files += 1
-			elif not dest in sent_files and docker_manager.send(dest, remotepath_dest):
-				ti.timeit(print, 'sent', dest, remotepath_dest)
-				sent_files.append(dest)
+			if not docker in docker_names: continue
+			docker_manager.rename(remotepath, root+dest)
 		else:
-			ti.timeit(print, f'ignoring {edit}')
-			return
+			print(f'ignoring {edit}')
 		updated_dockers.append(docker)
 	commits_done.append(commit_hash)
+
 st = time.time()
 for commit in commits:
 	process_commit(commit)
-nb_sent_files = len(sent_files)
 time_to_update_dockers = time.time() - st
 
 st = time.time()
@@ -90,26 +102,25 @@ st = time.time()
 # 	docker_manager.start(docker)
 time_to_restart_dockers = time.time() - st
 
-st = time.time()
+docker_manager.close()
+
 with open('commits_done', 'w+') as f:
 	for commit_hash in commits_done:
 		f.write(commit_hash+'\n')
-ti.time += time.time() - st
-ti.measured += 1
 
-docker_manager.close()
-time_spent_on_io = ti.time_spent_waiting()
-time_measuring_on_io = ti.time_spent_measuring()
 time_spent_on_ssh = docker_manager.ti.time_spent_waiting()
 time_measuring_on_ssh = docker_manager.ti.time_spent_measuring()
 time_total = time.time() - st_total
-time_other = time_total - time_spent_on_io - time_measuring_on_io - time_spent_on_ssh - time_measuring_on_ssh
+time_other = time_total - time_spent_on_ssh - time_measuring_on_ssh
+nb_updated_dockers = len(updated_dockers)
 print(	f"took {time_to_connect}s to connect\n"
 		f"took {time_to_update_dockers}s to update dockers\n"
 		f"took {time_to_restart_dockers}s to restart dockers\n\n"
-		f"spent {time_spent_on_io}s on io (+{time_measuring_on_io}s on measuring)\n"
 		f"spent {time_spent_on_ssh}s on ssh (+{time_measuring_on_ssh}s on measuring)\n\n"
-		f"spent {time_total}s total (of which {time_other}s not spent on io or ssh)\n\n"
-		f"sent {nb_sent_files} files\n"
-		f"removed {nb_removed_files} files\n"
-		f"moved {nb_moved_files} files")
+		f"spent {time_total}s total (of which {time_other}s not spent on ssh)\n\n"
+		f"updated {nb_updated_dockers} dockers\n"
+		f"sent {docker_manager.nb_send} files\n"
+		f"removed {docker_manager.nb_remove} files\n"
+		f"moved {docker_manager.nb_move} files\n"
+		f"renamed {docker_manager.nb_rename} files"
+		)
