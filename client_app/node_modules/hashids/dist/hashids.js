83e100288a3362bc85b4fa2814899b4dd44d35dd
Maxime <32194766+Pupariaa@users.noreply.github.com>
2024-02-13 00:59:52 +0100

    Delete client_app/node_modules directory

--- client_app/node_modules/hashids/dist/hashids.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-// this file here is for backwards compatibility with an earlier CommonJS version
-const Hashids = require("./index").default;
-Object.defineProperty(Hashids, "__esModule", {value: true});
-module.exports = Hashids;
-Hashids.default = module.exports;
