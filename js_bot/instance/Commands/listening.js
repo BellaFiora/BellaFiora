3fec90c2d2150c13cda5e89780c5e0dc2a44612d
lakazatong <bob.de-boisvilliers@epita.fr>
2024-02-15 19:34:56 +0100

    renamed folders, init drawio

--- js_bot/instance/Commands/listening.js
+++
@@ -1,9 +0,0 @@
-
-class Lst {
-  constructor() {
-  }
-  async execute(from, bmsetId, bmId, id) {
-    return { id: id, error: `0`, message :`Résultat de la commande`, output: null, filters: null, data: null};
-  }
-}
-module.exports = Lst;
