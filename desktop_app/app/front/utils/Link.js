31b558bd796b3abae7cc58db49d08b805e325125
Maxime <32194766+Pupariaa@users.noreply.github.com>
2024-04-18 14:42:03 +0200

    Create Link Function for opening URLs on client's navigator
    - Open Osu!Beatmap Page information
    - Copy Osu!Beatmap Link on the clipboard

---
+++ desktop_app/app/front/utils/Link.js
@@ -0,0 +1,33 @@
+const OsuUtils = require('../../lib/osu_utils')
+const { shell } = require('electron');
+let osuUtils = new OsuUtils()
+
+async function Link(event){
+    switch(event){
+        case'CopyLink':
+        let element = document.getElementById('shareBtn')
+        let beatmapId = element.getAttribute('data-beatmapId')
+        let beatmapsetId = element.getAttribute('data-beatmapsetId')
+        let mode = element.getAttribute('data-gamemode')
+        if(beatmapsetId === "-1"){
+            showNotificationBox('This beatmap is not published', "info")
+        } else {
+            await navigator.clipboard.writeText(`https://osu.ppy.sh/beatmapsets/${beatmapsetId}#${osuUtils.ModeIntToString(mode)}/${beatmapId}`);
+            showNotificationBox('Link copied', "info")
+        }
+        break;
+        case 'OpenBrowser':
+            let nelement = document.getElementById('shareBtn')
+            let nbeatmapId = nelement.getAttribute('data-beatmapId')
+            let nbeatmapsetId = nelement.getAttribute('data-beatmapsetId')
+            let nmode = nelement.getAttribute('data-gamemode')
+
+        if(nbeatmapsetId === "-1"){
+            showNotificationBox('This beatmap is not published', "info")
+        } else {
+            shell.openExternal(`https://osu.ppy.sh/beatmapsets/${nbeatmapsetId}#${osuUtils.ModeIntToString(nmode)}/${nbeatmapId}`); 
+        }
+    }
+}
+
+module.exports.Link = Link
